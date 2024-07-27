88e992f9828afbcbef9b01fb72b03532777a0c18
Frosty Gecko <imanfoster@gmail.com>
2024-07-27 12:02:45 -0600

    update

---
+++ Downloads/ESV/Genesis/Genesis 1:1-2:5.md
@@ -0,0 +1,51 @@
+
+# Genesis 1:1-2:5 (English Standard Version)
+The Creation of the World
+1:1 In the [^A]beginning, God created the heavens and the earth. 2 The earth was [^B]without form and void, and darkness was over the face of the deep. And the Spirit of God was hovering over the face of the waters.
+3 And God said, [^C]"Let there be light," and there was light. 4 And God saw that the light was good. And God separated the light from the darkness. 5 God called the light Day, and the darkness he called Night. And there was evening and there was morning, the first day.
+6 And God said, [^D]"Let there be an expanse in the midst of the waters, and let it separate the waters from the waters." 7 And God made the expanse and [^E]separated the waters that were under the expanse from the waters that were [^F]above the expanse. And it was so. 8 And God called the expanse Heaven. And there was evening and there was morning, the second day.
+9 And God said, [^G]"Let the waters under the heavens be gathered together into one place, and let the dry land appear." And it was so. 10 God called the dry land Earth, and the waters that were gathered together he called Seas. And God saw that it was good.
+11 And God said, [^H]"Let the earth sprout vegetation, plants yielding seed, and fruit trees bearing fruit in which is their seed, each according to its kind, on the earth." And it was so. 12 The earth brought forth vegetation, plants yielding seed according to their own kinds, and trees bearing fruit in which is their seed, each according to its kind. And God saw that it was good. 13 And there was evening and there was morning, the third day.
+14 And God said, "Let there be lights in the expanse of the heavens to separate the day from the night. And let them be for [^I]signs and for [^J]seasons, and for days and years, 15 and let them be lights in the expanse of the heavens to give light upon the earth." And it was so. 16 And God [^K]made the two great lights--the greater light to rule the day and the lesser light to rule the night--and the stars. 17 And God set them in the expanse of the heavens to give light on the earth, 18 to [^L]rule over the day and over the night, and to separate the light from the darkness. And God saw that it was good. 19 And there was evening and there was morning, the fourth day.
+20 And God said, "Let the waters swarm with swarms of living creatures, and let birds fly above the earth across the expanse of the heavens." 21 So [^M]God created the great sea creatures and every living creature that moves, with which the waters swarm, according to their kinds, and every winged bird according to its kind. And God saw that it was good. 22 And God blessed them, saying, [^N]"Be fruitful and multiply and fill the waters in the seas, and let birds multiply on the earth." 23 And there was evening and there was morning, the fifth day.
+24 And God said, "Let the earth bring forth living creatures according to their kinds--livestock and creeping things and beasts of the earth according to their kinds." And it was so. 25 And God made the beasts of the earth according to their kinds and the livestock according to their kinds, and everything that creeps on the ground according to its kind. And God saw that it was good.
+26 Then God said, [^O]"Let us make man in our image, [^P]after our likeness. And [^Q]let them have dominion over the fish of the sea and over the birds of the heavens and over the livestock and over all the earth and over every creeping thing that creeps on the earth."
+27 So God created man in his own image,  
+    in the image of God he created him;  
+    [^R]male and female he created them.
+28 And God blessed them. And God said to them, [^S]"Be fruitful and multiply and fill the earth and subdue it, and have dominion over the fish of the sea and over the birds of the heavens and over every living thing that moves on the earth." 29 And God said, "Behold, I have given you every plant yielding seed that is on the face of all the earth, and every tree with seed in its fruit. [^T]You shall have them for food. 30 And [^U]to every beast of the earth and to every bird of the heavens and to everything that creeps on the earth, everything that has the breath of life, I have given every green plant for food." And it was so. 31 [^V]And God saw everything that he had made, and behold, it was very good. And there was evening and there was morning, the sixth day.The Seventh Day, God Rests
+2:1 Thus the heavens and the earth were finished, and [^W]all the host of them. 2 And [^X]on the seventh day God finished his work that he had done, and he rested on the seventh day from all his work that he had done. 3 So God blessed the seventh day and made it holy, because on it God rested from all his work that he had done in creation.The Creation of Man and Woman
+4 [^Y]These are the generations  
+of the heavens and the earth when they were created,  
+in the day that the LORD God made the earth and the heavens.
+5 When no [^Z]bush of the field was yet in the land and no small plant of the field had yet sprung up--for the LORD God had not caused it to rain on the land, and there was no man [^AA]to work the ground,
+
+### Crossrefs
+[^A]: Job 38:4-7; Ps. 33:6; 136:5; Isa. 42:5; 45:18; John 1:1-3; Acts 14:15; 17:24; Col. 1:16, 17; Heb. 1:10; 11:3; Rev. 4:11
+[^B]: Jer. 4:23
+[^C]: 2 Cor. 4:6
+[^D]: Job 37:18; Ps. 136:5; Jer. 10:12; 51:15
+[^E]: Prov. 8:27-29
+[^F]: Ps. 148:4
+[^G]: Job 38:8-11; Ps. 33:7; 136:6; Jer. 5:22; 2 Pet. 3:5
+[^H]: Ps. 104:14
+[^I]: Jer. 10:2; Ezek. 32:7, 8; Joel 2:30, 31; 3:15; Matt. 24:29; Luke 21:25
+[^J]: Ps. 104:19
+[^K]: Deut. 4:19; Ps. 136:7-9
+[^L]: Jer. 31:35
+[^M]: Ps. 104:25, 26
+[^N]: ch. 8:17; 9:1
+[^O]: ch. 3:22; 11:7; Isa. 6:8
+[^P]: ch. 5:1; 9:6; 1 Cor. 11:7; Eph. 4:24; Col. 3:10; James 3:9
+[^Q]: ch. 9:2; Ps. 8:6-8; James 3:7
+[^R]: ch. 2:18, 21-23; 5:2; Mal. 2:15; Matt. 19:4; Mark 10:6
+[^S]: ch. 9:1, 7
+[^T]: ch. 9:3; Ps. 104:14, 15; 145:15, 16
+[^U]: Ps. 147:9
+[^V]: Eccles. 7:29; 1 Tim. 4:4
+[^W]: Deut. 4:19; Ps. 33:6
+[^X]: Ex. 20:8-11; 31:17; Deut. 5:12-14; Heb. 4:4
+[^Y]: ch. 1:1
+[^Z]: [ch. 1:11, 12]
+[^AA]: ch. 3:23
+
--- bible_download.py
+++ bible_download.py
@@ -7,25 +7,176 @@ Created on Thu Jul 25 00:14:57 2024
 """
 
 import os
+import pandas as pd
+from tqdm import tqdm
 
-#bg2md -b -c -e -r -v NIV "Jn 3.16-17" > passage.md
-
-def example_download():
+def BibleGatewayReference_example_download():
+    # '''
+    # Option | Option (longer form) | Meaning
+    # --------- | ------------ | ---------------------------------
+    # -b | --boldwords  |  Make the words of Jesus be shown in bold
+    # -c | --copyright  |  Exclude copyright notice from output
+    # -e | --headers |  Exclude editoria l headers from output
+    # -f | --footnotes  |  Exclude footnotes from output
+    # -h | --help  | Show help
+    # -i | --info |  Show information as I work
+    # -l | --newline | Start chapters and verses on a new line that starts with an H5 or H6 heading
+    # -n | --numbering  | Exclude verse and chapter numbers from output
+    # -r | --crossrefs  |  Exclude cross-refs from output
+    # -t | --test FILENAME  | Pass HTML from FILENAME instead of live lookup. 'reference' must still be given, but will be ignored.
+    # -v | --version VERSION | Select Bible version to lookup using BibleGateway's abbreviations (default:NET)
+    # '''
+    
+    #bg2md -b -c -e -r -v NIV "Jn 3.16-17" > passage.md
+    download_directory = 'ExampleDownload/'
+    if not os.path.exists(download_directory):
+        os.makedirs(download_directory)
+        
     options     = "-b -c -e -f -v"
-    book_abbrev = "GEN"
+    book_abbrev = "Genesis"
     chapter     = 1
     reference   = book_abbrev+" "+str(chapter)
     version     = "NASB"
     file        = "Genesis1.md"
+    path        = download_directory+file
+    initial     = "BibleGateway-to-Markdown/bg2md.rb "
+    execute     = initial+options+" "+version+" \""+reference+"\" > "+path
     
-    def download_bible(options,version,reference,file):
-        initial = "ruby bg2md.rb "
+    os.system(execute)
+    
+def download_bible(options,version,suffix=""):
+    bible_index = pd.read_csv("bible_index.csv")
+    books       = list(bible_index.columns)
+    books.remove('Chapter')
+    
+    for book in tqdm(books):
+        chapter_verse_count = bible_index[book]
+        verse_count         = list(filter(lambda num: num != 0, chapter_verse_count))
+        chapter_count       = len(verse_count)
         
-        execute = initial+options+" "+version+" \""+reference+"\" > "+file
-        
-        os.system(execute)
-        print(execute)
-        
-    download_bible(options,version,reference,file)
+        for chapter in range(1,chapter_count+1):
+            reference   = book+" "+str(chapter)
+            directory   = 'Downloads/'+version+suffix+"/"+book+"/"
 
-example_download()
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+                
+            filename    = reference+'.md'
+            path        = directory+filename
+            initial     = "BibleGateway-to-Markdown\ruby bg2md.rb "
+            
+            execute = initial+options+" "+version+" \""+reference+"\" > \""+path+"\""
+
+            os.system(execute)
+
+def BibleGatewayReference(version,
+                           book,
+                           reference,
+                           boldwords    = False,
+                           copyright    = False,
+                           headers      = False,
+                           footnotes    = False,
+                           newline      = False,
+                           crossrefs    = False,
+                           numbering    = True,
+                           verbose      = False,
+                           **args):
+    
+    # '''
+    # Option | Option (longer form) | Meaning
+    # --------- | ------------ | ---------------------------------
+    # -b | --boldwords  |  Make the words of Jesus be shown in bold
+    # -c | --copyright  |  Exclude copyright notice from output
+    # -e | --headers |  Exclude editoria l headers from output
+    # -f | --footnotes  |  Exclude footnotes from output
+    # -h | --help  | Show help
+    # -i | --info |  Show information as I work
+    # -l | --newline | Start chapters and verses on a new line that starts with an H5 or H6 heading
+    # -n | --numbering  | Exclude verse and chapter numbers from output
+    # -r | --crossrefs  |  Exclude cross-refs from output
+    # -t | --test FILENAME  | Pass HTML from FILENAME instead of live lookup. 'reference' must still be given, but will be ignored.
+    # -v | --version VERSION | Select Bible version to lookup using BibleGateway's abbreviations (default:NET)
+    # '''
+    
+    
+    #### Construct Options
+    if boldwords is True: opt1 = "-b " 
+    elif boldwords is False: opt1 = ""
+    
+    if copyright is True: opt2 = "" 
+    elif copyright is False: opt2 = "-c "
+    
+    if headers is True: opt3 = "" 
+    elif headers is False: opt3 = "-e "
+    
+    if footnotes is True: opt4 = "" 
+    elif footnotes is False: opt4 = "-f "
+    
+    if newline is True: opt5 = "-l "
+    elif newline is False: opt5 = "" 
+    
+    if numbering is True: opt6 = ""
+    elif numbering is False: opt6 = "-n " 
+    
+    if crossrefs is True: opt7 = ""
+    elif crossrefs is False: opt7 = "-r " 
+    
+    options             = opt1+opt2+opt3+opt4+opt5+opt6+opt7+'-v'
+        
+    #### Build Reference Markdown File
+    reference           = book+" "+reference
+    filename            = reference+'.md'
+    
+    #### Download Directory
+    download_folder     = 'Downloads/' 
+    downloaddir         = download_folder+version+"/"+book+"/"
+    savepath            = downloaddir+filename
+    
+    #### Check Existance of Download Folder
+    try:
+        if not os.path.exists(download_folder):
+            os.makedirs(download_folder)
+    except:
+        print("ERROR: Unable to create download folder") 
+        
+    #### Check Existance of Download Directory
+    try:
+        if not os.path.exists(downloaddir):
+            os.makedirs(downloaddir)
+    except:
+        print("ERROR: Unable to create download directory") 
+        
+    #### Build Execute Statement
+    # Build bg2md.rb file from BibleGateway-to-Markdown directory
+    initial             = "BibleGateway-to-Markdown/bg2md.rb "    
+    
+    # Build complete execution statement
+    execute             = initial+options+" "+version+" \""+reference+"\" > \""+savepath+"\""
+    
+    #### Printing
+    def debug(verbose):
+        if verbose is True:
+            print("Options: "+options)
+            print("Download Folder: "+download_folder)
+            print("Download Directory: "+downloaddir)
+            print("Save Path: "+savepath)
+            print("Execute Call: "+execute)     
+    
+    #### Execute
+    try:
+        os.system(execute)
+        debug(verbose)
+        return True
+    except:
+        print("ERROR: Could not execute bg2md.rb")
+        debug(True)
+        return False
+        
+
+#%% Download Reference
+
+book            = "Genesis"
+reference       = "1:1-2:5"
+version         = "ESV"
+
+BibleGatewayReference(version,book,reference,crossrefs=True)
